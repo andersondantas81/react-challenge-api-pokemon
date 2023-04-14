0052f1c5b124402ace2395e927bfae2f0e3d8446
andersondantas81 <andersondantas81@gmail.com>
2023-04-14 12:43:02 -0300

    implementação da paginação

--- src/App.jsx
+++ src/App.jsx
@@ -21,14 +21,35 @@ EXTRA: se puder ordene por nome.
 import axios from 'axios';
 import { useEffect, useState } from "react";
 
+const LIMIT = 20;
 function App() {
   const [list, setList] = useState([]);
-  const [pag, setPag] = useState(20)
-
+  const [pag, setPag] = useState(0);
+  
   const fetchListData = () => {
-    axios.get(`https://pokeapi.co/api/v2/pokemon/?offset=${pag}&limit=${pag+20}`)
+    axios.get('https://pokeapi.co/api/v2/pokemon')
     .then((response) => {
-      const sortedArray = [...response.data.results]
+      const sortedArray = [...response.data.results];
+
+      sortedArray.sort((a, b) => {
+        return a.name.localeCompare(b.name);
+      });
+
+      console.log(sortedArray);
+      const promisesrray = sortedArray.map((item) => {
+        return axios.get(item.url);
+      });
+
+      Promise.all(promisesrray).then((values) => setList(values));
+      
+      //setList(sortedArray);
+    });
+  }
+
+  const fetchListDataPaged = () => {
+    axios.get(`https://pokeapi.co/api/v2/pokemon/?offset=${pag}&limit=${pag + LIMIT}`)
+    .then((response) => {
+      const sortedArray = [...response.data.results];
 
       sortedArray.sort((a, b) => {
         return a.name.localeCompare(b.name);
@@ -49,18 +70,27 @@ function App() {
     fetchListData();
   }, []);
 
+  useEffect(() => {
+    fetchListDataPaged();
+  }, [pag]);
+ 
+  const handlerOnClick = (e) => {console.log(e);
+    if (e.target.innerText === "< previous" && pag !==0 ) setPag(pag - LIMIT);
+    if (e.target.innerText === "next >"  ) setPag(pag + LIMIT);
+  };
+
   return (
     <>
-      <h1>consumir api pokémon</h1>
+      <h1>Consumir api pokémon</h1>
       <hr />
       {list.length === 0 && "Carregando pokemon...."}
       {list.map((item) => (
         <Pokemon key={item.data.name} data={item.data} />
       ))}
       <hr />
-      <span >{'< previous'}</span>
+      <span onClick={handlerOnClick}>{'< previous'}</span>
       <span> | </span>
-      <span>{'next >'}</span>
+      <span onClick={handlerOnClick}>{'next >'}</span>
     </>
   );
 }
--- src/paginacao.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const MAX_ITEMS = 1279;
-
-const Paginacao = ({ limit, total, offset }) => {
-    return <div></div>
-};
-
-export default Paginacao;
